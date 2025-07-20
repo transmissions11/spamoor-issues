8447869f3320a47b3678a276aaad6b18e890e064
t11s <greenfeatherhelp@gmail.com>
2025-07-19 18:06:20 -0700

    Update start-reth.sh

--- start-reth.sh
+++ start-reth.sh
@@ -7,15 +7,24 @@ reth node --http --http.addr 0.0.0.0 \
 --dev --dev.block-time 1s \
 --engine.disable-precompile-cache \
 --builder.gaslimit 3000000000 \
+--builder.max-tasks 8 \
+--builder.deadline 1 \
 --txpool.pending-max-count 10000000000000 \
 --txpool.basefee-max-count 10000000000000 \
+--txpool.pending-max-size 10000000000000 \
+--txpool.basefee-max-size 10000000000000 \
 --txpool.queued-max-count 10000000000000 \
+--txpool.queued-max-size 10000000000000 \
 --txpool.max-account-slots 10000000000000 \
 --txpool.max-pending-txns 10000000000000 \
 --txpool.max-new-txns 10000000000000 \
+--txpool.max-new-pending-txs-notifications 10000000000000 \
+--txpool.disable-transactions-backup \
 --txpool.additional-validation-tasks 8 \
---rpc.max-connections max \
---max-tx-reqs 1000000 \
+--txpool.minimal-protocol-fee 0 \
+--txpool.minimum-priority-fee 0 \
+--rpc.max-connections 10000000000000 \
 --rpc.max-request-size 1000000 \
 --rpc.max-response-size 1000000 \
+--max-tx-reqs 1000000 \
 --chain dev.json
