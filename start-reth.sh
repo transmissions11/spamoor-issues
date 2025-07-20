295bbb22e5d6845578100b5a765229f465a62aee
t11s <greenfeatherhelp@gmail.com>
2025-07-19 17:52:37 -0700

    perf

--- start-reth.sh
+++ start-reth.sh
@@ -1,14 +1,12 @@
-node clear-reth-dev-db.js
+rm -rf /home/ubuntu/.local/share/reth/
 
-ulimit -n unlimited
-
-echo $(ulimit -n)
+ulimit -n 1000000
 
 reth node --http --http.addr 0.0.0.0 \
 --http.port 8545 --http.api all \
 --dev --dev.block-time 1s \
 --engine.disable-precompile-cache \
---builder.gaslimit 10000000000000 \
+--builder.gaslimit 3000000000 \
 --txpool.pending-max-count 10000000000000 \
 --txpool.basefee-max-count 10000000000000 \
 --txpool.queued-max-count 10000000000000 \
@@ -16,7 +14,7 @@ reth node --http --http.addr 0.0.0.0 \
 --txpool.max-pending-txns 10000000000000 \
 --txpool.max-new-txns 10000000000000 \
 --txpool.additional-validation-tasks 8 \
- --rpc.max-connections max \
+--rpc.max-connections max \
 --max-tx-reqs 1000000 \
 --rpc.max-request-size 1000000 \
 --rpc.max-response-size 1000000 \
